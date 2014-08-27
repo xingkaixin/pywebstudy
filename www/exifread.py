4c927ce411c1d7b970169387a86dca32d77d0b94
xingkaixin <xingkaixin@gmail.com>
2014-08-27 20:12:04 +0800

    update

--- www/exifread.py
+++ www/exifread.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 __author__ = 'XingKaiXin.me'
-
 import exifread
 
 f = open(r'C:\ideaproject\pyXingKaiXin.me\www\a.jpg', 'rb')
@@ -34,13 +33,14 @@ def gpstran(a):
 	j = []
 	for i in a:
 		j.append(i.num/i.den)
-		print j
+		#print j
 	return j[0] + j[1]/60.0 + j[2]/3600.0
 
-gpsvalues = gpsdict[Latitude].values
-print gpsvalues
+gpsLatvalues = gpsdict[Latitude].values
+gpslongvalues = gpsdict[Longitude].values
+print gpsLatvalues,gpslongvalues
 # #print gpsvalues, gpsvalues[0],gpsvalues[1],dir(gpsvalues[2])
 # print gpsvalues[0],dir(gpsvalues[0]), gpsvalues[0].den,gpsvalues[0].num
 # print gpsvalues[1],dir(gpsvalues[1]), gpsvalues[1].den,gpsvalues[1].num
 # print gpsvalues[2],dir(gpsvalues[2]), gpsvalues[2].den,gpsvalues[2].num
-print gpstran(gpsvalues)
+print gpstran(gpsLatvalues),gpstran(gpslongvalues)
