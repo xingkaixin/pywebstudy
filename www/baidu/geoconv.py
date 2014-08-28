bbe5ef9b15345ea1da76febf8c26b9dbc5ea7c35
xingkaixin <xingkaixin@gmail.com>
2014-08-28 14:08:52 +0800

    update gerconv

--- www/baidu/geoconv.py
+++ www/baidu/geoconv.py
@@ -6,8 +6,13 @@ __author__ = 'XingKaiXin.me'
 import urllib
 import json
 
+url = r'http://api.map.baidu.com/geoconv/v1/?from=1&to=5'
+dash = r'&'
+ak = 'ak='+r'djfa2ui3jfdkafsd'
+coords = 'coords='+r'113.544961111,22.1887777778'
+
 args = urllib.urlencode([('num', 10), ('page', 0)])
-url = 'http://api.map.baidu.com/geoconv/v1/?coords=113.544961111,22.1887777778&from=1&to=5&ak=你的密钥'
+url = url + dash + ak+dash+coords
 data = urllib.urlopen(url, args)
 
 json_data = json.loads([i for i in data][0])
