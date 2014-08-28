4e737f0e556e729d8e343127f5b76527496382e5
xingkaixin <xingkaixin@gmail.com>
2014-08-28 11:11:09 +0800

    update gps

--- www/gps.py
+++ www/gps.py
@@ -5,44 +5,37 @@ from __future__ import division
 __author__ = 'XingKaiXin.me'
 
 import exifread
+import os
 
-f = open(r'./gpsimg/d.jpg', 'rb')
 
-Longitude = "GPS GPSLongitude"
-Latitude = "GPS GPSLatitude"
-isSouthorNorth = "GPS GPSLatitudeRef"
-isEastorWest = "GPS GPSLongitudeRef"
-
+Longitude, Latitude, isSouthorNorth, isEastorWest = "GPS GPSLongitude", "GPS GPSLatitude", \
+                                                    "GPS GPSLatitudeRef", "GPS GPSLongitudeRef"
 gps = [Longitude, Latitude, isSouthorNorth, isEastorWest]
-tags = exifread.process_file(f)
-f.close()
-gpsdict = {}
-for tag in tags.keys():
-    if tag in gps:
-        #print "Key: %s, value %s" % (tag, tags[tag])
-        gpsdict[tag] = tags[tag]
-    # else:
-    # 	print "Key: %s, value %s" % (tag, tags[tag])
-
-# def gpstran(a):
-# 	return a[0] + a[1]/60 + a[2]/3600
-
-# print gpsdict[Latitude], gpstran(gpsdict[Latitude])
-# print gpsdict[Longitude], gpstran(gpsdict[Longitude])
+files = [r'./gpsimg/a.jpg', r'./gpsimg/b.jpg', r'./gpsimg/c.jpg', r'./gpsimg/d.jpg']
 
 
-def gpstran(a):
-    j = []
-    for i in a:
-        j.append(i.num/i.den)
-        #print j
-    return j[0] + j[1]/60.0 + j[2]/3600.0
+def printgps(file):
+    filename = os.path.split(file)[1]
+    with open(file, 'rb') as f:
+        tags = exifread.process_file(f)
 
-gpsLatvalues = gpsdict[Latitude].values
-gpslongvalues = gpsdict[Longitude].values
-print gpsLatvalues, gpslongvalues
-# #print gpsvalues, gpsvalues[0],gpsvalues[1],dir(gpsvalues[2])
-# print gpsvalues[0],dir(gpsvalues[0]), gpsvalues[0].den,gpsvalues[0].num
-# print gpsvalues[1],dir(gpsvalues[1]), gpsvalues[1].den,gpsvalues[1].num
-# print gpsvalues[2],dir(gpsvalues[2]), gpsvalues[2].den,gpsvalues[2].num
-print gpstran(gpsLatvalues), gpstran(gpslongvalues)
+    gpsdict = {}
+    for tag in tags.keys():
+        if tag in gps:
+            gpsdict[tag] = tags[tag]
+
+
+    def gpstran(a):
+        j = []
+        for i in a:
+            j.append(i.num/i.den)
+        return j[0] + j[1]/60.0 + j[2]/3600.0
+
+    gpsisSourthorNorth = gpsdict[isSouthorNorth].values
+    gpsisEastorWest = gpsdict[isEastorWest].values
+    gpsLatvalues = gpsdict[Latitude].values
+    gpslongvalues = gpsdict[Longitude].values
+    print filename, gpsisEastorWest, gpstran(gpslongvalues), gpsisSourthorNorth, gpstran(gpsLatvalues)
+
+for f1 in files:
+    printgps(f1)
