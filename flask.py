7c5e07c18f9d3faa5c040e67a175cb79732a145e
xingkaixin <xingkaixin@gmail.com>
2014-08-28 14:25:52 +0800

    init

---
+++ flask.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'XingKaiXin.me'
+
+# all the imports
+
+
+# configuation
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
